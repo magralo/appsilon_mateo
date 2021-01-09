4da2eebd755fd217025406af7c1dd115eff49a53
magralo <magralo@gmail.com>
2021-01-09 03:44:02 -0500

    important point in indepht map

--- app.R
+++ app.R
@@ -98,10 +98,26 @@ inDServer <- function(id,name,sid) {
     output$map_all <- renderLeaflet({
       df=data()
       if(name()==df$shipname[1]&&sid()==df$ship_id [1]){
+        D=df%>%
+          mutate(dist=get_distance(lon,lat))%>%
+          filter(!is.na(dist))
+        
+        aux=which.max(D$dist)
+        aux= D[c((aux-1):(aux+1)),]
+        
+        
         leaflet() %>%
           addTiles() %>%  # use the default base map which is OpenStreetMap tiles
           addPolylines( lng= df$lon, 
-                        lat = df$lat)
+                        lat = df$lat)%>%
+          addCircleMarkers(lng=aux$lon[2], lat=aux$lat[2],
+                           popup="Origin max",color = "red") %>%
+          addCircleMarkers(lng=aux$lon[3], lat=aux$lat[3],
+                           popup="Destination max",color = "green")%>%
+          addCircleMarkers(lng=D$lon[1], lat=D$lat[1],
+                           popup="First Obs",color = "orange")%>%
+          addCircleMarkers(lng=D$lon[nrow(D)], lat=D$lat[nrow(D)],
+                           popup="last Obs",color = "blue")
       }else{
         leaflet() %>%
           addTiles() 
