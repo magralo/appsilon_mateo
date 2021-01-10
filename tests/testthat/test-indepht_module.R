c961f88eca7d90199c2ff908302136cf385158f9
magralo <magralo@gmail.com>
2021-01-10 00:02:14 -0500

    extra tests

--- tests/testthat/test-indepht_module.R
+++ tests/testthat/test-indepht_module.R
@@ -17,5 +17,31 @@ test_that("test the used sql", {
     expect_equal(sql_test(), sql_ok)
     
     
+    sname("DAR-10")
+    sid('3061268')
+    sql_test = inDServer ('test' ,name = sname,sid=sid)
+    
+    sql_ok = 'SELECT lat,lon,date,datetime, shipname,ship_id 
+              FROM `original-brace-297916.ships.ships` 
+              WHERE shipname = "DAR-10" AND ship_id= 3061268
+              ORDER BY datetime' 
+    
+    
+    expect_equal(sql_test(), sql_ok)
+    
+    
+    sname("DAR-20")
+    sid('3061267')
+    sql_test = inDServer ('test' ,name = sname,sid=sid)
+    
+    sql_ok = 'SELECT lat,lon,date,datetime, shipname,ship_id 
+              FROM `original-brace-297916.ships.ships` 
+              WHERE shipname = "DAR-20" AND ship_id= 3061267
+              ORDER BY datetime' 
+    
+    
+    expect_equal(sql_test(), sql_ok)
+    
+    
   }) 
 })
